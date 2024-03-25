c69365bc0a61f6a5e09ba7e0e10805b487cb589f
leopl <leopl@LEVANIDPC>
2024-03-25 13:33:30 +0300

    Добавьте файлы проекта.

---
+++ TestiriumWF/MainMenu.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TestiriumWF
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateTest
+            // 
+            this.btnCreateTest.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateTest.Name = "btnCreateTest";
+            this.btnCreateTest.Size = new System.Drawing.Size(205, 54);
+            this.btnCreateTest.TabIndex = 0;
+            this.btnCreateTest.Text = "Создать тестирование";
+            this.btnCreateTest.UseVisualStyleBackColor = true;
+            this.btnCreateTest.Click += new System.EventHandler(this.btnCreateTest_Click);
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 450);
+            this.Controls.Add(this.btnCreateTest);
+            this.Name = "MainMenu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateTest;
+    }
+}
+
---
+++ TestiriumWF/TestCreating.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace TestiriumWF
+{
+    partial class TestCreating
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblProgrammTitle = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.questionsFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnWelcomeScreen = new System.Windows.Forms.Button();
+            this.btnCreateQuestion = new System.Windows.Forms.Button();
+            this.btnOneQuestionAnswer = new System.Windows.Forms.Button();
+            this.welcomeScreenPanel = new System.Windows.Forms.Panel();
+            this.lblWelcomeScreenTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.questionsFlowLayoutPanel.SuspendLayout();
+            this.welcomeScreenPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblProgrammTitle
+            // 
+            this.lblProgrammTitle.AutoSize = true;
+            this.lblProgrammTitle.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblProgrammTitle.ForeColor = System.Drawing.Color.White;
+            this.lblProgrammTitle.Location = new System.Drawing.Point(12, 18);
+            this.lblProgrammTitle.Name = "lblProgrammTitle";
+            this.lblProgrammTitle.Size = new System.Drawing.Size(158, 42);
+            this.lblProgrammTitle.TabIndex = 0;
+            this.lblProgrammTitle.Text = "Тестириум";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.panel1.Controls.Add(this.questionsFlowLayoutPanel);
+            this.panel1.Controls.Add(this.btnCreateQuestion);
+            this.panel1.Location = new System.Drawing.Point(0, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(246, 557);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(71)))), ((int)(((byte)(71)))));
+            this.panel2.Controls.Add(this.lblProgrammTitle);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1084, 77);
+            this.panel2.TabIndex = 3;
+            // 
+            // questionsFlowLayoutPanel
+            // 
+            this.questionsFlowLayoutPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.questionsFlowLayoutPanel.AutoScroll = true;
+            this.questionsFlowLayoutPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.questionsFlowLayoutPanel.Controls.Add(this.btnWelcomeScreen);
+            this.questionsFlowLayoutPanel.Controls.Add(this.btnOneQuestionAnswer);
+            this.questionsFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.questionsFlowLayoutPanel.Location = new System.Drawing.Point(13, 85);
+            this.questionsFlowLayoutPanel.Name = "questionsFlowLayoutPanel";
+            this.questionsFlowLayoutPanel.Padding = new System.Windows.Forms.Padding(15);
+            this.questionsFlowLayoutPanel.Size = new System.Drawing.Size(221, 452);
+            this.questionsFlowLayoutPanel.TabIndex = 2;
+            this.questionsFlowLayoutPanel.WrapContents = false;
+            // 
+            // btnWelcomeScreen
+            // 
+            this.btnWelcomeScreen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.btnWelcomeScreen.FlatAppearance.BorderSize = 0;
+            this.btnWelcomeScreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWelcomeScreen.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnWelcomeScreen.ForeColor = System.Drawing.Color.White;
+            this.btnWelcomeScreen.Location = new System.Drawing.Point(18, 18);
+            this.btnWelcomeScreen.Name = "btnWelcomeScreen";
+            this.btnWelcomeScreen.Size = new System.Drawing.Size(165, 40);
+            this.btnWelcomeScreen.TabIndex = 3;
+            this.btnWelcomeScreen.Text = "Титульный экран";
+            this.btnWelcomeScreen.UseVisualStyleBackColor = false;
+            // 
+            // btnCreateQuestion
+            // 
+            this.btnCreateQuestion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.btnCreateQuestion.FlatAppearance.BorderSize = 0;
+            this.btnCreateQuestion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreateQuestion.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCreateQuestion.ForeColor = System.Drawing.Color.White;
+            this.btnCreateQuestion.Location = new System.Drawing.Point(12, 19);
+            this.btnCreateQuestion.Name = "btnCreateQuestion";
+            this.btnCreateQuestion.Size = new System.Drawing.Size(200, 50);
+            this.btnCreateQuestion.TabIndex = 1;
+            this.btnCreateQuestion.Text = "Добавить вопрос";
+            this.btnCreateQuestion.UseVisualStyleBackColor = false;
+            // 
+            // btnOneQuestionAnswer
+            // 
+            this.btnOneQuestionAnswer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.btnOneQuestionAnswer.FlatAppearance.BorderSize = 0;
+            this.btnOneQuestionAnswer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOneQuestionAnswer.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOneQuestionAnswer.ForeColor = System.Drawing.Color.White;
+            this.btnOneQuestionAnswer.Location = new System.Drawing.Point(18, 64);
+            this.btnOneQuestionAnswer.Name = "btnOneQuestionAnswer";
+            this.btnOneQuestionAnswer.Size = new System.Drawing.Size(165, 40);
+            this.btnOneQuestionAnswer.TabIndex = 4;
+            this.btnOneQuestionAnswer.Text = "Вопрос с одним ответом";
+            this.btnOneQuestionAnswer.UseVisualStyleBackColor = false;
+            // 
+            // welcomeScreenPanel
+            // 
+            this.welcomeScreenPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(81)))), ((int)(((byte)(81)))));
+            this.welcomeScreenPanel.Controls.Add(this.linkLabel1);
+            this.welcomeScreenPanel.Controls.Add(this.textBox2);
+            this.welcomeScreenPanel.Controls.Add(this.label2);
+            this.welcomeScreenPanel.Controls.Add(this.textBox1);
+            this.welcomeScreenPanel.Controls.Add(this.label1);
+            this.welcomeScreenPanel.Controls.Add(this.lblWelcomeScreenTitle);
+            this.welcomeScreenPanel.Location = new System.Drawing.Point(265, 94);
+            this.welcomeScreenPanel.Name = "welcomeScreenPanel";
+            this.welcomeScreenPanel.Size = new System.Drawing.Size(807, 518);
+            this.welcomeScreenPanel.TabIndex = 4;
+            // 
+            // lblWelcomeScreenTitle
+            // 
+            this.lblWelcomeScreenTitle.AutoSize = true;
+            this.lblWelcomeScreenTitle.Font = new System.Drawing.Font("Bahnschrift SemiBold SemiConden", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblWelcomeScreenTitle.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblWelcomeScreenTitle.Location = new System.Drawing.Point(24, 25);
+            this.lblWelcomeScreenTitle.Name = "lblWelcomeScreenTitle";
+            this.lblWelcomeScreenTitle.Size = new System.Drawing.Size(153, 25);
+            this.lblWelcomeScreenTitle.TabIndex = 0;
+            this.lblWelcomeScreenTitle.Text = "Титульный экран";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(26, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Название";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.textBox1.Location = new System.Drawing.Point(29, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(746, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Введите название теста";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Bahnschrift Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(159)))), ((int)(((byte)(159)))));
+            this.textBox2.Location = new System.Drawing.Point(29, 175);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(746, 257);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Введите описание для теста (здесь может быть краткая информация, необходимая обуч" +
+    "ающемуся до тестирования)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Light", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(26, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Описание для теста";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.White;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Bahnschrift Light", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(26, 435);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(176, 14);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Добавить картинку или видео";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.White;
+            // 
+            // TestCreating
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(61)))), ((int)(((byte)(61)))));
+            this.ClientSize = new System.Drawing.Size(1084, 632);
+            this.Controls.Add(this.welcomeScreenPanel);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TestCreating";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TestCreating";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.questionsFlowLayoutPanel.ResumeLayout(false);
+            this.welcomeScreenPanel.ResumeLayout(false);
+            this.welcomeScreenPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblProgrammTitle;
+        private System.Windows.Forms.Button btnCreateQuestion;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnWelcomeScreen;
+        private System.Windows.Forms.FlowLayoutPanel questionsFlowLayoutPanel;
+        private System.Windows.Forms.Button btnOneQuestionAnswer;
+        private System.Windows.Forms.Panel welcomeScreenPanel;
+        private System.Windows.Forms.Label lblWelcomeScreenTitle;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
